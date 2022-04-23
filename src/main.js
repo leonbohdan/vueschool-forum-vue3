74b57eea0a302c15c8e1ef335154ab0261aae3a5
Bohdan Leonets <bohdan@mytowerapp.com>
2022-04-23 18:31:56 +0300

    Dynamic Route Matching & Route Params

--- src/main.js
+++ src/main.js
@@ -2,12 +2,19 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 import PageHome from '@/components/PageHome'
+import PageThreadShow from '@/components/PageThreadShow'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: PageHome
+  },
+  {
+    path: '/thread/:id',
+    name: 'ThreadShow',
+    component: PageThreadShow,
+    props: true
   }
 ]
 
