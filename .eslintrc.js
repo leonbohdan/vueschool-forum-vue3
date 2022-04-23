24b3bb6961a232ef949785ec36909bdda3d31de5
Bohdan Leonets <bohdan@mytowerapp.com>
2022-04-23 16:35:00 +0300

    eslint with vue/standard preset was added

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard'
+  ],
+
+  root: true,
+
+  env: {
+    node: true
+  },
+
+  parserOptions: {
+    parser: '@babel/eslint-parser'
+  },
+
+  rules: {
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 2 : 1,
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
